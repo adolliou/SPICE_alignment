corrected sign in PCi_j matrix

SPICE_alignment/hdrshift/alignement.py
@@ -130,6 +130,9 @@ class Alignment:
             crot = self.crota_ref + kwargs["d_crota"]
             # raise NotImplementedError
         if ((('d_cdelta1' in kwargs.keys()) or ('d_cdelta2' in kwargs.keys()) or ('d_crota' in kwargs.keys()))):
+            print(f'{self.crota_ref=}')
+            print(f'{crot=}')
+
             rho = np.deg2rad(crot)
             lam = hdr["CDELT2"] / hdr["CDELT1"]
             hdr["PC1_1"] = np.cos(rho)
@@ -310,7 +313,8 @@ class Alignment:
         elif 'CROTA2' in self.hdr_small:
             self.crota_ref = self.hdr_small['CROTA2']
         else:
-            self.crota_ref = np.rad2deg(np.arccos(self.hdr_small['PC1_1']))
+            s = - np.sign(self.hdr_small['PC1_2'])
+            self.crota_ref = np.rad2deg(np.arccos(self.hdr_small['PC1_1'])) * s
             self.hdr_small["CROTA"] = np.rad2deg(np.arccos(self.hdr_small['PC1_1']))
             # self.use_crota = False
         self.cdelta1_ref = self.hdr_small['CDELT1']

SPICE_alignment/plot/plot.py
@@ -467,13 +467,13 @@ class PlotFunctions:
                 if parameter_alignment['cdelt1'] is not None:
                     cdelt1 = u.Quantity(hdr_spice_shifted["CDELT1"], hdr_spice_shifted["CUNIT1"]) + \
                                                   u.Quantity(parameter_alignment['cdelt1'][max_index[2]], "arcsec")
-                    hdr_spice_shifted["CDELT1"] = cdelt1.to(hdr_spice_shifted["CUNIT1"]).value
+                    hdr_spice_shifted["CDELT1"] += cdelt1.to(hdr_spice_shifted["CUNIT1"]).value
                     change_pcij = True
 
                 if parameter_alignment['cdelt2'] is not None:
                     cdelt2 = u.Quantity(hdr_spice_shifted["CDELT2"], hdr_spice_shifted["CUNIT2"]) + \
                              u.Quantity(parameter_alignment['cdelt2'][max_index[3]], "arcsec")
-                    hdr_spice_shifted["CDELT2"] = cdelt2.to(hdr_spice_shifted["CUNIT2"]).value
+                    hdr_spice_shifted["CDELT2"] += cdelt2.to(hdr_spice_shifted["CUNIT2"]).value
                     change_pcij = True
                 if change_pcij:
                     s = - np.sign(hdr_spice_shifted["PC1_2"])