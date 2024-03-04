corrected sign in PCi_j matrix

SPICE_alignment/hdrshift/alignement.py
@@ -231,10 +231,10 @@ class Alignment:
 
         self.data_large = np.array(f_large[self.large_fov_window].data.copy(), dtype=np.float64)
         self.hdr_large = f_large[self.large_fov_window].header.copy()
-        self._recenter_crpix_in_header(self.hdr_large)
+        # self._recenter_crpix_in_header(self.hdr_large)
 
         self.hdr_small = f_small[self.small_fov_window].header.copy()
-        self._recenter_crpix_in_header(self.hdr_small)
+        # self._recenter_crpix_in_header(self.hdr_small)
         self.data_small = np.array(f_small[self.small_fov_window].data.copy(), dtype=np.float64)
 
         if (lonlims is None) and (latlims is None) & (size_deg_carrington is not None):
@@ -276,10 +276,10 @@ class Alignment:
         self.data_large = dat_large_var
 
         self.hdr_large = f_large[self.large_fov_window].header.copy()
-        self._recenter_crpix_in_header(self.hdr_large)
+        # self._recenter_crpix_in_header(self.hdr_large)
 
         self.hdr_small = f_small[self.small_fov_window].header.copy()
-        self._recenter_crpix_in_header(self.hdr_small)
+        # self._recenter_crpix_in_header(self.hdr_small)
         self.data_small = np.array(f_small[self.small_fov_window].data.copy(), dtype=np.float64)
         f_large.close()
         f_small.close()
@@ -292,28 +292,28 @@ class Alignment:
 
         naxis1, naxis2 = self._get_naxis(self.hdr_large)
 
-        if (self.hdr_large["CRPIX1"] != (naxis1 + 1) / 2) or (self.hdr_large["CRPIX2"] != (naxis2 + 1) / 2):
-            crpix1_old = self.hdr_large["CRPIX1"]
-            crpix2_old = self.hdr_large["CRPIX2"]
-            self._recenter_crpix_in_header(self.hdr_large)
-            crpix1_new = self.hdr_large["CRPIX1"]
-            crpix2_new = self.hdr_large["CRPIX2"]
-            warnings.warn(f"in hdr_large : CRPIX1 or 2 not in center of FOV."
-                          f"\nReplacing CRPIX1 from {crpix1_old} to {crpix1_new}"
-                          f"\nReplacing CRPIX2 from {crpix2_old} to {crpix2_new}")
+        # if (self.hdr_large["CRPIX1"] != (naxis1 + 1) / 2) or (self.hdr_large["CRPIX2"] != (naxis2 + 1) / 2):
+        #     crpix1_old = self.hdr_large["CRPIX1"]
+        #     crpix2_old = self.hdr_large["CRPIX2"]
+        #     self._recenter_crpix_in_header(self.hdr_large)
+        #     crpix1_new = self.hdr_large["CRPIX1"]
+        #     crpix2_new = self.hdr_large["CRPIX2"]
+        #     warnings.warn(f"in hdr_large : CRPIX1 or 2 not in center of FOV."
+        #                   f"\nReplacing CRPIX1 from {crpix1_old} to {crpix1_new}"
+        #                   f"\nReplacing CRPIX2 from {crpix2_old} to {crpix2_new}")
 
         naxis1, naxis2 = self._get_naxis(self.hdr_small)
 
-        if (self.hdr_small["CRPIX1"] != (naxis1 + 1) / 2) or (self.hdr_small["CRPIX2"] != (naxis2 + 1) / 2):
-            crpix1_old = self.hdr_small["CRPIX1"]
-            crpix2_old = self.hdr_small["CRPIX2"]
-            self._recenter_crpix_in_header(self.hdr_small)
-            crpix1_new = self.hdr_small["CRPIX1"]
-            crpix2_new = self.hdr_small["CRPIX2"]
-
-            warnings.warn(f"in hdr_small : CRPIX1 or 2 not in center of FOV."
-                          f"\nReplacing CRPIX1 from {crpix1_old} to {crpix1_new}"
-                          f"\nReplacing CRPIX2 from {crpix2_old} to {crpix2_new}")
+        # if (self.hdr_small["CRPIX1"] != (naxis1 + 1) / 2) or (self.hdr_small["CRPIX2"] != (naxis2 + 1) / 2):
+        #     crpix1_old = self.hdr_small["CRPIX1"]
+        #     crpix2_old = self.hdr_small["CRPIX2"]
+        #     self._recenter_crpix_in_header(self.hdr_small)
+        #     crpix1_new = self.hdr_small["CRPIX1"]
+        #     crpix2_new = self.hdr_small["CRPIX2"]
+        #
+        #     warnings.warn(f"in hdr_small : CRPIX1 or 2 not in center of FOV."
+        #                   f"\nReplacing CRPIX1 from {crpix1_old} to {crpix1_new}"
+        #                   f"\nReplacing CRPIX2 from {crpix2_old} to {crpix2_new}")
 
         self.crval1_ref = self.hdr_small['CRVAL1']
         self.crval2_ref = self.hdr_small['CRVAL2']
@@ -333,10 +333,10 @@ class Alignment:
         self.unit1 = self.hdr_small["CUNIT1"]
         self.unit2 = self.hdr_small["CUNIT2"]
 
-        if (self.hdr_large["CRPIX1"] !=
-            (self.hdr_large["NAXIS1"] + 1) / 2) or (self.hdr_large["CRPIX2"] !=
-                                                    (self.hdr_large["NAXIS2"] + 1) / 2):
-            raise ValueError("in hdr_large : CRPIX1 or 2 not in center of FOV")
+        # if (self.hdr_large["CRPIX1"] !=
+        #     (self.hdr_large["NAXIS1"] + 1) / 2) or (self.hdr_large["CRPIX2"] !=
+        #                                             (self.hdr_large["NAXIS2"] + 1) / 2):
+        #     raise ValueError("in hdr_large : CRPIX1 or 2 not in center of FOV")
 
         if "arcsec" in self.unit1:
             warnings.warn("Unit of headers in arcsec : must provide arcsec units for dcrval")
@@ -466,20 +466,20 @@ class Alignment:
         shmm_correlation.unlink()
         return data_correlation_cp
 
-    def _recenter_crpix_in_header(self, hdr):
-        w = WCS(hdr)
-        naxis1, naxis2 = self._get_naxis(hdr)
-        x_mid = (naxis1 - 1) / 2
-        y_mid = (naxis2 - 1) / 2
-        lon_mid, lat_mid = w.pixel_to_world(np.array([x_mid]), np.array([y_mid]))
-        lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
-        lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
-        hdr["CRVAL1"] = lon_mid
-        hdr["CRVAL2"] = lat_mid
-        hdr["CRPIX1"] = (naxis1 + 1) / 2
-        hdr["CRPIX2"] = (naxis2 + 1) / 2
-
-        return hdr
+    # def _recenter_crpix_in_header(self, hdr):
+    #     w = WCS(hdr)
+    #     naxis1, naxis2 = self._get_naxis(hdr)
+    #     x_mid = (naxis1 - 1) / 2
+    #     y_mid = (naxis2 - 1) / 2
+    #     lon_mid, lat_mid = w.pixel_to_world(np.array([x_mid]), np.array([y_mid]))
+    #     lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
+    #     lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
+    #     hdr["CRVAL1"] = lon_mid
+    #     hdr["CRVAL2"] = lat_mid
+    #     hdr["CRPIX1"] = (naxis1 + 1) / 2
+    #     hdr["CRPIX2"] = (naxis2 + 1) / 2
+    #
+    #     return hdr
 
     def _carrington_transform(self, d_solar_r, data, hdr):
 

SPICE_alignment/hdrshift/alignement_spice.py
@@ -81,7 +81,7 @@ class AlignmentSpice(Alignment):
         with fits.open(self.large_fov_known_pointing) as hdul_large:
             self.data_large = np.array(hdul_large[self.large_fov_window].data.copy(), dtype=np.float64)
             self.hdr_large = hdul_large[self.large_fov_window].header.copy()
-            super()._recenter_crpix_in_header(self.hdr_large)
+            # super()._recenter_crpix_in_header(self.hdr_large)
             hdul_large.close()
 
     def _extract_spice_data_header(self, level: int, index_amplitude=None):
@@ -145,17 +145,24 @@ class AlignmentSpice(Alignment):
 
         w_xy = w_xyt.dropaxis(2)
         self.hdr_small = w_xy.to_header().copy()
-        self.hdr_small["NAXIS1"] = data_small.shape[3]
-        self.hdr_small["NAXIS2"] = data_small.shape[2]
-        super()._recenter_crpix_in_header(self.hdr_small)
-        ylen = data_small.shape[2]
+        # self.hdr_small["NAXIS1"] = data_small.shape[3]
+        # self.hdr_small["NAXIS2"] = data_small.shape[2]
+        # super()._recenter_crpix_in_header(self.hdr_small)
+        # ylen = data_small.shape[2]
 
-        ylim = np.array([ymin, ylen - ymax - 1]).max()
-        self.data_small = np.nansum(data_small[0, :, ylim:(ylen - ylim), :], axis=0)
+        # ylim = np.array([ymin, ylen - ymax - 1]).max()
+        data_small[:, :, :ymin, :] = np.nan
+        data_small[:, :, ymax:, :] = np.nan
 
-        self.hdr_small["CRPIX1"] = (self.data_small.shape[1] + 1) / 2
-        self.hdr_small["CRPIX2"] = (self.data_small.shape[0] + 1) / 2
+        self.data_small = np.nansum(data_small[0, :, :, :], axis=0)
+        self.data_small[:ymin, :] = np.nan
+        self.data_small[ymax:, :] = np.nan
 
+
+        #
+        # self.hdr_small["CRPIX1"] = (self.data_small.shape[1] + 1) / 2
+        # self.hdr_small["CRPIX2"] = (self.data_small.shape[0] + 1) / 2
+        #
         self.hdr_small["NAXIS1"] = self.data_small.shape[1]
         self.hdr_small["NAXIS2"] = self.data_small.shape[0]
 

SPICE_alignment/plot/plot.py
@@ -400,28 +400,32 @@ class PlotFunctions:
             header_large = hdul_large[large_fov_window].header.copy()
             data_large = hdul_large[large_fov_window].data.copy()
             with fits.open(small_fov_path) as hdul_spice:
-                header_spice = hdul_spice[small_fov_window].header.copy()
+                header_spice_original = hdul_spice[small_fov_window].header.copy()
 
-                if "HRI_EUV" in  header_spice["TELESCOP"]:
-                    AlignEUIUtil.recenter_crpix_in_header(header_spice)
-                    w_xy = WCS(header_spice)
+                if "HRI_EUV" in  header_spice_original["TELESCOP"]:
+                    # AlignEUIUtil.recenter_crpix_in_header(header_spice)
+                    w_xy = WCS(header_spice_original)
                     header_spice = w_xy.to_header().copy()
                     data_spice = np.array(hdul_spice[small_fov_window].data.copy(), dtype=np.float64)
-                elif  "SPICE" in header_spice["TELESCOP"]:
-                    AlignSpiceUtil.recenter_crpix_in_header_L2(header_spice)
-                    w_spice = WCS(header_spice)
-                    ymin, ymax = AlignSpiceUtil.vertical_edges_limits(header_spice)
+                elif  "SPICE" in header_spice_original["TELESCOP"]:
+                    # AlignSpiceUtil.recenter_crpix_in_header_L2(header_spice)
+                    w_spice = WCS(header_spice_original)
+                    ymin, ymax = AlignSpiceUtil.vertical_edges_limits(header_spice_original)
                     w_xyt = w_spice.dropaxis(2)
                     w_xyt.wcs.pc[2, 0] = 0
                     w_xy = w_xyt.dropaxis(2)
                     header_spice = w_xy.to_header().copy()
 
                     data_small = np.array(hdul_spice[small_fov_window].data.copy(), dtype=np.float64)
-                    ylen = data_small.shape[2]
-                    ylim = np.array([ymin, ylen - ymax - 1]).max()
-                    data_spice = np.nansum(data_small[0, :, ylim:(ylen - ylim), :], axis=0)
-                    header_spice["CRPIX1"] = (data_spice.shape[1] + 1) / 2
-                    header_spice["CRPIX2"] = (data_spice.shape[0] + 1) / 2
+                    data_small[:, :, :ymin, :] = np.nan
+                    data_small[:, :, ymax:, :] = np.nan
+
+                    data_spice = np.nansum(data_small[0, :, :, :], axis=0)
+                    data_spice[:ymin, :] = np.nan
+                    data_spice[ymax:, :] = np.nan
+
+                    # header_spice["CRPIX1"] = (data_spice.shape[1] + 1) / 2
+                    # header_spice["CRPIX2"] = (data_spice.shape[0] + 1) / 2
 
 
 
@@ -433,11 +437,15 @@ class PlotFunctions:
 
                 header_spice["NAXIS1"] = data_spice.shape[1]
                 header_spice["NAXIS2"] = data_spice.shape[0]
-                AlignEUIUtil.recenter_crpix_in_header(header_spice)
+                # AlignEUIUtil.recenter_crpix_in_header(header_spice)
                 not_nan = np.isnan(data_spice)
                 levels = [np.percentile(data_spice[~not_nan], n) for n in levels_percentile]
 
                 hdr_spice_shifted = header_spice.copy()
+                if header_spice_original["PC1_1"] == 1.0:
+                    for i, j in zip([1, 1, 2, 2], [1, 2, 1, 2]):
+                        hdr_spice_shifted[f'PC{i}_{j}'] = header_spice_original[f'PC{i}_{j}']
+
                 hdr_spice_shifted["CRVAL1"] = hdr_spice_shifted["CRVAL1"] \
                                               + u.Quantity(parameter_alignment['crval1'][max_index[0]], "arcsec").to(
                     hdr_spice_shifted["CUNIT1"]).value
@@ -451,6 +459,7 @@ class PlotFunctions:
                     key_rota = "CROTA"
                 elif "CROTA2" in hdr_spice_shifted:
                     key_rota = "CROTA2"
+
                 crota = np.rad2deg(np.arccos(copy.deepcopy(hdr_spice_shifted["PC1_1"])))
 
                 if parameter_alignment['crota'] is not None:
@@ -488,7 +497,7 @@ class PlotFunctions:
                 max = np.percentile(data_large[~not_nan], 99)
                 norm = ImageNormalize(stretch=LinearStretch(), vmin=np.max((min, 1)), vmax=max)
 
-                longitude, latitude = AlignEUIUtil.extract_EUI_coordinates(header_spice, dsun=False)
+                longitude, latitude = AlignEUIUtil.extract_EUI_coordinates(header_spice.copy(), dsun=False)
                 longitude_grid, latitude_grid, dlon, dlat = PlotFits.build_regular_grid(longitude, latitude)
                 dlon = dlon.to("arcsec").value
                 dlat = dlat.to("arcsec").value

SPICE_alignment/utils/Util.py
@@ -103,12 +103,12 @@ class AlignCommonUtil:
 
                 hdu = hdul[window]
                 hdr = hdu.header
-                if ("EUI" in hdr["TELESCOP"]) or ("AIA" in hdr["TELESCOP"]):
-                    AlignEUIUtil.recenter_crpix_in_header(hdul[window].header)
-                elif "SPICE" in hdr["TELESCOP"]:
-                    AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window].header)
-                else:
-                    raise NotImplementedError
+                # if ("EUI" in hdr["TELESCOP"]) or ("AIA" in hdr["TELESCOP"]):
+                #     AlignEUIUtil.recenter_crpix_in_header(hdul[window].header)
+                # elif "SPICE" in hdr["TELESCOP"]:
+                #     AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window].header)
+                # else:
+                #     raise NotImplementedError
 
                 change_pcij = False
                 if lag_crval1 is not None:
@@ -215,22 +215,23 @@ class AlignEUIUtil:
 
     @staticmethod
     def recenter_crpix_in_header(hdr):
-        w = WCS(hdr)
-        if "ZNAXIS1" in hdr:
-            naxis1 = hdr["ZNAXIS1"]
-            naxis2 = hdr["ZNAXIS2"]
-        else:
-            naxis1 = hdr["NAXIS1"]
-            naxis2 = hdr["NAXIS2"]
-        x_mid = (naxis1 - 1) / 2
-        y_mid = (naxis2 - 1) / 2
-        lon_mid, lat_mid = w.pixel_to_world(np.array([x_mid]), np.array([y_mid]))
-        lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
-        lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
-        hdr["CRVAL1"] = lon_mid
-        hdr["CRVAL2"] = lat_mid
-        hdr["CRPIX1"] = (naxis1 + 1) / 2
-        hdr["CRPIX2"] = (naxis2 + 1) / 2
+        pass
+        # w = WCS(hdr)
+        # if "ZNAXIS1" in hdr:
+        #     naxis1 = hdr["ZNAXIS1"]
+        #     naxis2 = hdr["ZNAXIS2"]
+        # else:
+        #     naxis1 = hdr["NAXIS1"]
+        #     naxis2 = hdr["NAXIS2"]
+        # x_mid = (naxis1 - 1) / 2
+        # y_mid = (naxis2 - 1) / 2
+        # lon_mid, lat_mid = w.pixel_to_world(np.array([x_mid]), np.array([y_mid]))
+        # lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
+        # lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
+        # hdr["CRVAL1"] = lon_mid
+        # hdr["CRVAL2"] = lat_mid
+        # hdr["CRPIX1"] = (naxis1 + 1) / 2
+        # hdr["CRPIX2"] = (naxis2 + 1) / 2
 
     @staticmethod
     def write_corrected_fits(path_eui_l2_input: str, window_eui, path_eui_l2_output: str, corr: np.array,
@@ -244,7 +245,7 @@ class AlignEUIUtil:
         with fits.open(path_eui_l2_input) as hdul:
             # hdu = hdul[window_spice]
             # hdr_shifted = hdu.header
-            AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window_eui].header)
+            # AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window_eui].header)
             change_pcij = False
             if lag_crval1 is not None:
                 hdul[window_eui].header['CRVAL1'] = hdul[window_eui].header['CRVAL1'
@@ -405,30 +406,34 @@ class AlignSpiceUtil:
 
     @staticmethod
     def recenter_crpix_in_header_L2(hdr):
-        w = WCS(hdr)
-        w_xyt = w.dropaxis(2)
+        pass
+        # w = WCS(hdr)
+        # w_xyt = w.dropaxis(2)
+        #
+        # if "ZNAXIS1" in hdr:
+        #     naxis1 = hdr["ZNAXIS1"]
+        #     naxis2 = hdr["ZNAXIS2"]
+        #     naxis3 = hdr["ZNAXIS3"]
+        # else:
+        #     naxis1 = hdr["NAXIS1"]
+        #     naxis2 = hdr["NAXIS2"]
+        #     naxis3 = hdr["NAXIS3"]
+        #
+        # x_mid = (naxis1 - 1) / 2
+        # y_mid = (naxis2 - 1) / 2
+        # t_mid = (naxis3 - 1) / 2
+        #
+        # lon_mid, lat_mid, utc_mid = w_xyt.pixel_to_world(np.array([x_mid]), np.array([y_mid]), np.array(t_mid))
+        # lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
+        # lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
+        #
+        # hdr["CRVAL1"] = lon_mid
+        # hdr["CRVAL2"] = lat_mid
+        # hdr["CRPIX1"] = (naxis1 + 1) / 2
+        # hdr["CRPIX2"] = (naxis2 + 1) / 2
 
-        if "ZNAXIS1" in hdr:
-            naxis1 = hdr["ZNAXIS1"]
-            naxis2 = hdr["ZNAXIS2"]
-            naxis3 = hdr["ZNAXIS3"]
-        else:
-            naxis1 = hdr["NAXIS1"]
-            naxis2 = hdr["NAXIS2"]
-            naxis3 = hdr["NAXIS3"]
 
-        x_mid = (naxis1 - 1) / 2
-        y_mid = (naxis2 - 1) / 2
-        t_mid = (naxis3 - 1) / 2
-
-        lon_mid, lat_mid, utc_mid = w_xyt.pixel_to_world(np.array([x_mid]), np.array([y_mid]), np.array(t_mid))
-        lon_mid = lon_mid[0].to(hdr["CUNIT1"]).value
-        lat_mid = lat_mid[0].to(hdr["CUNIT2"]).value
-
-        hdr["CRVAL1"] = lon_mid
-        hdr["CRVAL2"] = lat_mid
-        hdr["CRPIX1"] = (naxis1 + 1) / 2
-        hdr["CRPIX2"] = (naxis2 + 1) / 2
+        # must also shift the rotation
 
     @staticmethod
     def extract_l3_data(path_spice: str, line: dict, index_line: int, window=0):
@@ -464,7 +469,7 @@ class AlignSpiceUtil:
             for window_spice in window_spice_list:
                 # hdu = hdul[window_spice]
                 # hdr_shifted = hdu.header
-                AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window_spice].header)
+                # AlignSpiceUtil.recenter_crpix_in_header_L2(hdul[window_spice].header)
                 change_pcij = False
                 if lag_crval1 is not None:
                     hdul[window_spice].header['CRVAL1'] = hdul[window_spice].header['CRVAL1'