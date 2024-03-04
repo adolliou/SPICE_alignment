corrected sign in PCi_j matrix

SPICE_alignment/hdrshift/alignement.py
@@ -124,6 +124,8 @@ class Alignment:
             else:
                 if kwargs["d_crota"] != 0.0:
                     crot = np.rad2deg(np.arccos(hdr["PC1_1"]))
+                    s = - np.sign(hdr["PC1_2"])
+                    crot = crot * s
                     hdr["CROTA"] = crot
             crot = self.crota_ref + kwargs["d_crota"]
             # raise NotImplementedError
@@ -132,8 +134,8 @@ class Alignment:
             lam = hdr["CDELT2"] / hdr["CDELT1"]
             hdr["PC1_1"] = np.cos(rho)
             hdr["PC2_2"] = np.cos(rho)
-            hdr["PC1_2"] = lam * np.sin(rho)
-            hdr["PC2_1"] = - (1 / lam) * np.sin(rho)
+            hdr["PC1_2"] = - lam * np.sin(rho)
+            hdr["PC2_1"] = (1 / lam) * np.sin(rho)
 
     def _iteration_step_along_crval2(self, d_crval1, d_cdelta1, d_cdelta2, d_crota, d_solar_r, method: str,
                                      position: tuple, lock=None):

SPICE_alignment/synras/map_builder.py
@@ -134,8 +134,11 @@ class ComposedMapBuilder(MapBuilder):
                                                      hdr_im["CUNIT2"]).to(self.hdr_composed["CUNIT2"]).value
             lam = self.hdr_composed["CDELT2"] / self.hdr_composed["CDELT1"]
             rho = np.arccos(self.hdr_composed["PC1_1"])
-            self.hdr_composed["PC1_2"] = lam * np.sin(rho)
-            self.hdr_composed["PC2_1"] = -(1 / lam) * np.sin(rho)
+            s = - np.sign(self.hdr_composed["PC1_2"])
+            rho = rho * s
+
+            self.hdr_composed["PC1_2"] = - lam * np.sin(rho)
+            self.hdr_composed["PC2_1"] = (1 / lam) * np.sin(rho)
             self.hdr_composed["CRPIX1"] = (self.data_composed.shape[1] + 1) / 2
             self.hdr_composed["CRPIX2"] = (self.data_composed.shape[0] + 1) / 2
             #

SPICE_alignment/utils/Util.py
@@ -122,6 +122,8 @@ class AlignCommonUtil:
                         hdul[window].header['CUNIT2']).value
                 key_rota = None
                 crota = np.rad2deg(np.arccos(hdul[window].header["PC1_1"]))
+                s = - np.sign(hdul[window].header["PC1_2"])
+                crota = crota * s
                 if "CROTA" in hdul[window].header:
                     key_rota = "CROTA"
                 elif "CROTA2" in hdul[window].header:
@@ -149,8 +151,8 @@ class AlignCommonUtil:
                     lam = hdul[window].header["CDELT2"] / hdul[window].header["CDELT1"]
                     hdul[window].header["PC1_1"] = np.cos(theta)
                     hdul[window].header["PC2_2"] = np.cos(theta)
-                    hdul[window].header["PC1_2"] = lam * np.sin(theta)
-                    hdul[window].header["PC2_1"] = - (1 / lam) * np.sin(theta)
+                    hdul[window].header["PC1_2"] = - lam * np.sin(theta)
+                    hdul[window].header["PC2_1"] = (1 / lam) * np.sin(theta)
 
             hdul.writeto(path_l2_output, overwrite=True)
             hdul.close()