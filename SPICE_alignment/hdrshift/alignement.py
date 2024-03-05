corrected sign in PCi_j matrix

SPICE_alignment/hdrshift/alignement.py
@@ -448,7 +448,8 @@ class Alignment:
                     Processes[ii].start()
                     while (np.sum([p.is_alive() for p in Processes]) > self.counts):
                         pass
-                    [p.close() for p in Processes if not (p.is_alive())]
+
+                    [p.close() for kk, p in zip(range(lenp), Processes) if ((~(p.is_alive()) and kk <= ii))]
                 while (np.sum([p.is_alive() for p in Processes]) != 0):
                     pass
                 for P in Processes: