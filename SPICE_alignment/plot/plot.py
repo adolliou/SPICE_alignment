corrected sign in PCi_j matrix

SPICE_alignment/plot/plot.py
@@ -504,7 +504,7 @@ class PlotFunctions:
                     hdr_spice_shifted["CDELT2"] = cdelt2.to(hdr_spice_shifted["CUNIT2"]).value
                     change_pcij = True
                 if change_pcij:
-                    s = - np.sign(hdr_spice_shifted["PC1_2"])
+                    s = - np.sign(hdr_spice_shifted["PC1_2"]) + (hdr_spice_shifted["PC1_2"] == 0.0)
                     theta = np.deg2rad(crota) * s
                     lam = hdr_spice_shifted["CDELT2"] / hdr_spice_shifted["CDELT1"]
                     hdr_spice_shifted["PC1_1"] = np.cos(theta)